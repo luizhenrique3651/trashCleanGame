1e703730085ae0d87f0487061603748e5744795d
luizhenrique3651 <uizhenrique@live.com>
2019-10-20 16:48:15 -0400

    Arquivos Adicionados
    Game com estrutura lógica pronta, restando apenas as imagens

---
+++ src/gamedecisivo/escolha.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gamedecisivo;
+
+/**
+ *
+ * @author luiz
+ */
+public class escolha {
+    
+    
+    
+  public void  escolhendo(int escolhaDaOpcao){
+    if(escolhaDaOpcao == 1){
+            fase2_investigar  ignorar = new fase2_investigar();
+            ignorar.setVisible(true);
+            
+        }
+    
+    }
+}
